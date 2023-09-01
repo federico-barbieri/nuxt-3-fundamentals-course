9bc6ffab8c20697c1231c488229fc13e76867372
Federico Barbieri <federicobarbieri54@gmail.com>
2023-09-01 23:56:50 +0200

    personal

--- pages/projects.vue
+++ pages/projects.vue
@@ -7,12 +7,12 @@
         <ul class="project-list">
 
                     <button @click="toggleKea">KEA</button>
-                    <button>PERSONAL PROJECTS</button>
+                    <button @click="togglePersonal">PERSONAL PROJECTS</button>
                     
                     <ProjectCard class="messiverse" v-if="kea" name="Messiverse" description="a cool project" img="/images/hogwarts.png" />
-                    <ProjectCard name="Magic Hackers" description="a cool project" img="" />
-                    <ProjectCard name="Jimmy Sakurai" description="a cool project" img="" />
-                    <ProjectCard name="SwampFest" description="a cool project" img="" />
+                    <ProjectCard name="Magic Hackers" v-if="kea" description="a cool project" img="" />
+                    <ProjectCard name="Jimmy Sakurai" v-if="personal" description="a cool project" img="" />
+                    <ProjectCard name="SwampFest" v-if="kea" description="a cool project" img="" />
 
                     
                     </ul>
@@ -26,6 +26,12 @@ const kea = ref(false);
 
 function toggleKea(){
     kea.value = !kea.value;
+}
+
+const personal = ref(false);
+
+function togglePersonal(){
+    personal.value = !personal.value;
 }
 
 </script>
