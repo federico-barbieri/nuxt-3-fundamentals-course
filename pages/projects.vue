b3519dd5f6045cb3d9a52259c7a98be826ccdc18
Federico Barbieri <federicobarbieri54@gmail.com>
2023-09-02 00:04:04 +0200

    all projects

--- pages/projects.vue
+++ pages/projects.vue
@@ -8,11 +8,12 @@
 
                     <button @click="toggleKea">KEA</button>
                     <button @click="togglePersonal">PERSONAL PROJECTS</button>
+                    <button @click="toggleAll">ALL PROJECTS</button>
                     
                     <ProjectCard class="messiverse" v-if="kea" name="Messiverse" description="a cool project" img="/images/hogwarts.png" />
-                    <ProjectCard name="Magic Hackers" v-if="kea" description="a cool project" img="" />
-                    <ProjectCard name="Jimmy Sakurai" v-if="personal" description="a cool project" img="" />
-                    <ProjectCard name="SwampFest" v-if="kea" description="a cool project" img="" />
+                    <ProjectCard class="hackers" name="Magic Hackers" v-if="kea" description="a cool project" img="" />
+                    <ProjectCard class="sakurai" name="Jimmy Sakurai" v-if="personal" description="a cool project" img="" />
+                    <ProjectCard class="swampfest" name="SwampFest" v-if="kea" description="a cool project" img="" />
 
                     
                     </ul>
@@ -25,13 +26,26 @@
 const kea = ref(false);
 
 function toggleKea(){
-    kea.value = !kea.value;
+    kea.value = true;
+    if(personal.value){
+        personal.value = false;
+    }
 }
 
 const personal = ref(false);
 
 function togglePersonal(){
-    personal.value = !personal.value;
+    personal.value = true;
+    if(kea.value){
+        kea.value = false;
+    }
+}
+
+const allProjects = ref(false);
+
+function toggleAll(){
+    kea.value = true;
+    personal.value = true;
 }
 
 </script>
@@ -40,6 +54,18 @@ function togglePersonal(){
 
 .messiverse{
     background-color: black;
+}
+
+.hackers{
+    background-color: hotpink;
+}
+
+.sakurai{
+    background-color: aqua;
+}
+
+.swampfest{
+    background-color: saddlebrown;
 }
 
 </style>
