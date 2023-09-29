7ca1ebd237d54524388caea7a3f8c6d2f17194a9
Federico Barbieri <federicobarbieri54@gmail.com>
2023-09-29 16:41:58 +0200

    cleanup

--- pages/swampfest.vue
+++ pages/swampfest.vue
@@ -3,11 +3,19 @@
 
         <main>
 
+            <!-- HERO SECTION -->
+
             <section class="hero">
+
+                <!-- TITLE CONTAINER -->
 
                 <div class="title-container">
 
-                    <h1><span class="federico">SWAMPFEST</span></h1>
+                    <div class="title-inner-div">
+                        <h1><span class="federico">SWAMPFEST</span></h1>
+                        <span>Made in collaboration with Sabrina Sørensen</span>
+
+                    </div>
 
                     <a :href="`#${scrollToElementId}`">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right arrow-name" viewBox="0 0 16 16">
@@ -18,23 +26,27 @@
 
                 </div>
 
+                <!-- BURGER MENU CONTAINER -->
+
 
                 <div class="burger-container" :id="scrollToElementId">
         
                     <img class="burger-image" src="/swampfest-gifs/burger.gif" alt="Gif of burger menu display">
                     
-                    <p class="about-p">
+                    <p>
                     This Shrek-inspired music festival<br>
                     is the final project of my 3rd semester at KEA.<br>
                     </p>
 
-                </div>    
+                </div>
+                
+                <!-- SCHEDULE CONTAINER -->
 
                 <div class="schedule-container">
 
                     <img class="schedule-image" src="/swampfest-gifs/schedule.gif" alt="Gif of schedule page">
 
-                    <p class="about-p">
+                    <p>
                     The platform fetches information from an api<br>
                     and updates the schedule for three different stages<br>
                     in real-time.
@@ -43,11 +55,13 @@
 
                 </div>
 
+                <!-- BANDS CONTAINER -->
+
                 <div class="bands-container">
 
                     <img class="bands-image" src="/swampfest-gifs/bands.gif" alt="Gif of bands page">
 
-                    <p class="about-p">
+                    <p>
                     You can also find a list of the bands<br>
                     that will be playing, their image, description<br>
                     and band members.
@@ -56,11 +70,13 @@
 
                 </div>
 
+                <!-- TICKETS CONTAINER -->
+
                 <div class="tickets-container">
 
                     <img class="tickets-image" src="/swampfest-gifs/ticket1.gif" alt="Gif of ticket step">
 
-                    <p class="about-p">
+                    <p>
                     The buying flow for tickets consists of 5 steps:<br>
                     tickets, camping add-ons, ticket-holder-information<br>
                     delivery and order review.
@@ -69,11 +85,13 @@
 
                 </div>
 
+                <!-- SUPABASE CONTAINER -->
+
                 <div class="supabase-container">
 
                     <img class="supabase-image" src="/swampfest-gifs/supabase.png" alt="Supabase image">
 
-                    <p class="about-p">
+                    <p>
                     The project is connected to Supabase,<br>
                     so all the information is received<br>
                     in the correct format.
@@ -82,12 +100,16 @@
 
                 </div>
 
+                <!-- PROCESS TITLE -->
 
-                <h2 class="projects-title">PROCESS</h2>
+
+                <h2 class="process-title"></h2>
+
+                <!-- PROCESS CONTAINER -->
 
                 <div class="process-container">
 
-                    <p class="about-p">
+                    <p>
                         After doing research on current trends<br>
                         and famous music festivals,<br>
                         we agreed on the different sections of the website. 
@@ -123,19 +145,6 @@
 
 <script setup>
 
-import Button from "../components/Button.vue";
-import stack from "../assets/stack.json"
-import Name from "../components/Name.vue";
-import ProjectSquare from "../components/ProjectSquare.vue";
-import Random from "../components/Random.vue";
-import Dog from "../components/Dog.vue";
-import NiceBackground from "../components/NiceBackground.vue";
-
-
-
-
-let stackArray = stack.stack;
-
 let scrollToElementId = 'burger-container';
 
 
@@ -146,12 +155,25 @@ let scrollToElementId = 'burger-container';
 
 <style scoped>
 
+/* LINKS */
+
 a{
     color: inherit;
 }
 
 a:visited{
     color: inherit;
+}
+
+/* TEXT */
+
+p{
+    font-size: 1.5rem;
+    margin: 0rem 0 0 2rem;
+    width: 40rem;
+    height: 50%;
+    line-height: 2;
+    text-align: left;
 }
 
 /* MAIN */
@@ -169,7 +191,7 @@ main{
     overflow-y: hidden;
 }
 
-/* HERO */
+/* HERO SECTION */
 
 .hero{
     display: flex;
@@ -183,11 +205,20 @@ main{
     
 }
 
+/* TITLE CONTAINER */
+
 .title-container{
     width: 80vw;
     display: flex;
     flex-direction: row;
     align-items: center;
+}
+
+.title-inner-div{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
 }
 
 h1{
@@ -199,6 +230,9 @@ h1{
     transform: scale(6);
     margin-left: 7rem;
 }
+
+
+/* BURGER CONTAINER */
 
 .burger-container{
     width: 90vw;
@@ -223,6 +257,8 @@ h1{
     transform: scale(0.8);
 }
 
+/* SCHEDULE CONTAINER */
+
 .schedule-container{
     width: 90vw;
     height: 80vh;
@@ -245,6 +281,9 @@ h1{
     border-bottom-right-radius: 30px;
     transform: scale(0.8);
 }
+
+
+/* BANDS CONTAINER */
 
 .bands-container{
     width: 90vw;
@@ -269,6 +308,9 @@ h1{
     transform: scale(0.8);
 }
 
+
+/* TICKETS CONTAINER */
+
 .tickets-container{
     width: 90vw;
     height: 80vh;
@@ -291,6 +333,9 @@ h1{
     border-bottom-right-radius: 30px;
     transform: scale(0.8);
 }
+
+
+/* SUPABASE CONTAINER */
 
 .supabase-container{
     width: 90vw;
@@ -315,8 +360,21 @@ h1{
     transform: scale(0.8);
 }
 
+/* PROCESS TITLE */
+
+.process-title{
+    writing-mode: vertical-rl; /* vertical writing mode, right-to-left */
+    text-orientation: upright; /* characters are upright */
+    font-size: 5rem;
+    opacity: 0.3;
+    margin: 0 5vw;
+}
+
+
+/* PROCESS CONTAINER */
+
 .process-container{
-    width: 300vw;
+    width: 250vw;
     height: 80vh;
     background-color: transparent;
     border-bottom-left-radius: 30px;
@@ -354,207 +412,14 @@ h1{
     margin-bottom: 15rem;
 }
 
-h2{
-    font-size: 6rem;
-    opacity: 0.3;
-    margin: 0 5vw;
-}
-
-h3{
-    font-size: 3rem;
-    opacity: 0.3;
-    margin: 0 5vw;
-}
-
-/* ABOUT */
-.about-title{
-    writing-mode: vertical-rl; /* vertical writing mode, right-to-left */
-    text-orientation: upright; /* characters are upright */
-}
-
-.about-p{
-    font-size: 1.5rem;
-    margin: 0rem 0 0 2rem;
-    width: 40rem;
-    height: 50%;
-    line-height: 2;
-    display: flex;
-    align-items: center;
-}
-
-
-.projects-title{
-    writing-mode: vertical-rl; /* vertical writing mode, right-to-left */
-    text-orientation: upright; /* characters are upright */
-    font-size: 5rem;
-}
-
-.p-and-ul{
-    width: 70%;
-    height: 100%;
-    text-align: center;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-around;
-}
-
-.projects-p{
-    font-size: 1.5rem;
-}
-
-/* list of projects */
-.project-list{
-    list-style-type: none;
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-template-rows: 1fr 1fr;
-    height: 35%;
-    width: 100%;
-}
-
-.project-img{
-    max-width: 100%;
-    height: auto;
-    border-radius: 5px;
-    filter: drop-shadow(5px 5px 5px black);
-}
-
-/* STACK */
-
-.stack{
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-around;
-    height: 250vh;
-    width: 100%;
-    padding: 1rem;
-    margin: 1rem;
-}
-
-.stack-title{
-    writing-mode: vertical-rl; /* vertical writing mode, right-to-left */
-    text-orientation: upright; /* characters are upright */
-    width: 30%;
-}
-
-.p-and-ul-stack{
-    width: 70%;
-    height: 100%;
-    text-align: center;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-}
-
-.stack-p{
-    font-size: 1.5rem;
-    max-width: 50%;
-    margin: 0 0 10rem 0;
-}
-
-/* list of projects */
-.stack-list{
-    list-style-type: none;
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    grid-template-rows: repeat(6, 1fr);
-    height: 60%;
-    width: 100%;
-    gap: 1rem;
-}
-
-.iconGallery{
-    margin-right: 10rem;
-}
-
-/* CONTACT */
-
-.contact{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-    width: 100%;
-}
+/* CONTACT CONTAINER */
 
 .contact-title{
-    
     text-decoration: overline;
+    font-size: 3rem;
+    opacity: 0.3;
+    margin: 0 2rem 0 0;
 }
 
-.swampfest{
-    transition: all 0.5s ease-in;
-}
-
-.swampfest:hover{
-    background-image: url('/projectImgs/festival1.jpg');
-    background-position: center;
-    background-size: cover;
-    background-repeat: no-repeat;
-    backface-visibility: hidden;
-    color: black;
-    font-weight: 700;
-}
-
-
-.messiverse{
-    transition: all 0.5s ease-in;
-}
-
-.messiverse:hover{
-    background-image: url('/messi/xul1.png');
-    background-position: center;
-    background-size: cover;
-    background-repeat: no-repeat;
-    backface-visibility: hidden;
-    color: black;
-    font-weight: 700;
-}
-
-.sakurai{
-    transition: all 0.5s ease-in;
-}
-
-.sakurai:hover{
-    background-image: url('/projectImgs/moon.png');
-    background-position: center;
-    background-size: cover;
-    background-repeat: no-repeat;
-    backface-visibility: hidden;
-    color: black;
-    font-weight: 700;
-}
-
-.hogwarts{
-    transition: all 0.5s ease-in;
-}
-
-.hogwarts:hover{
-    background-image: url('/projectImgs/fire-hogwarts.png');
-    background-position: center;
-    background-size: cover;
-    background-repeat: no-repeat;
-    backface-visibility: hidden;
-    color: black;
-    font-weight: 700;
-}
-
-.edc{
-    transition: all 0.5s ease-in;
-}
-
-.edc:hover{
-    background-image: url('/projectImgs/ecd.jpg');
-    background-position: center;
-    background-size: cover;
-    background-repeat: no-repeat;
-    backface-visibility: hidden;
-    color: black;
-    font-weight: 700;
-}
 
 </style>
